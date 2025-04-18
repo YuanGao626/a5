beb3cebd49b4f5ce5f6c34f4be79a2e568e8345a
ivanzheng <ivannzheng@gmail.com>
2025-04-17 21:25:37 -0400

    most recent

--- src/cs2110/TwoPassIndexer.java
+++ src/cs2110/TwoPassIndexer.java
@@ -1,7 +1,9 @@
 package cs2110;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * An Indexer that uses a sorted array plus binary search to serve as a string map.  Each source is
@@ -18,7 +20,46 @@ public class TwoPassIndexer extends Indexer {
         //  search to update the appropriate one as each word is re-read from the sources.  You may
         //  only use methods declared in `IndexedSeq`, `WordOccurrences`, `Scanner`/`WordScanner`,
         //  `String`, `FileReader`, and `Iterable`/`Iterator`, as well as `Sorting.binarySearch()`.
-        throw new UnsupportedOperationException();
+        IndexedSeq<String> words = makeIndexedSeq();
+
+        for (String file : sortedSrcNames){
+            try (Scanner scanner = new Scanner(new File(file))) {
+                while (scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    for (String word : line.split("\\s+")){
+                        if (!word.isEmpty()){
+                            words.add(word.toUpperCase());
+                        }
+                    }
+                }
+            }
+        }
+
+        words.sortDistinct();
+
+        IndexedSeq<WordOccurrences> occurrences = makeIndexedSeq();
+        for(int i=0; i < words.size(); i++){
+            occurrences.add(new WordOccurrences(words.get(i)));
+        }
+
+        for (String file : sortedSrcNames){
+            try (Scanner scanner = new Scanner(new File(file))){
+                int lineIndex = 0;
+                while (scanner.hasNextLine()){
+                    lineIndex++;
+                    String line = scanner.nextLine();
+                    for (String word : line.split("\\s+")){
+                        if(!word.isEmpty()){
+                            int index = Sorting.binarySearch(words, word.toUpperCase());
+                            occurrences.get(index).addOccurrence(file, lineIndex);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return occurrences;
     }
 
     /**
